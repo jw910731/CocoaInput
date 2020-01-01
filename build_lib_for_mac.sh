bf9bea94ed997d70ef8b4b3c48f2ac662a43f0c9
粉圓 <jw910731@gmail.com>
2020-01-01 14:04:02 +0800

    build native library when gradle build

--- build_lib_for_mac.sh
+++ build_lib_for_mac.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Build libcocoainput for macOS"
+[ -d "src/main/resources/assets/darwin" ] && mkdir src/main/resources/assets/darwin
 cd libcocoainput/libcocoainput
 make && make install
